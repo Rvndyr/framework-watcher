a8f8f9e2df199e005a5b2ecdf9cb9bda0bf6ab2d
Randy Rodriguez <Randyr2125@msn.com>
2021-08-09 09:05:24 -0400

    Added github api routes and table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,49 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <b-container>
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Repo</th>
+            <th scope="col">Starred</th>
+            <th scope="col">Forked</th>
+            <th scope="col">Watchers</th>
+            <th scope="col">Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>{{ this.repos[0].name }}</td>
+            <td>{{ this.repos[0].stargazers_count }}</td>
+            <td>{{ this.repos[0].forks }}</td>
+            <td>{{ this.repos[0].watchers_count }}</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>{{ this.repos[1].name }}</td>
+            <td>{{ this.repos[1].stargazers_count }}</td>
+            <td>{{ this.repos[1].forks }}</td>
+            <td>{{ this.repos[1].watchers_count }}</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>{{ this.repos[2].name }}</td>
+            <td>{{ this.repos[2].stargazers_count }}</td>
+            <td>{{ this.repos[2].forks }}</td>
+            <td>{{ this.repos[2].watchers_count }}</td>
+          </tr>
+          <tr>
+            <th scope="row">4</th>
+            <td>{{ this.repos[3].name }}</td>
+            <td>{{ this.repos[3].stargazers_count }}</td>
+            <td>{{ this.repos[3].forks }}</td>
+            <td>{{ this.repos[3].watchers_count }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </b-container>
   </div>
 </template>
 
@@ -11,15 +54,29 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
-      repo: null,
+      repos: [],
     };
   },
   created: function () {
     axios.get("https://api.github.com/repos/vuejs/vue").then((response) => {
-      console.log(response);
-      this.repo = response.data;
-      console.log("This is the data:", this.repo);
+      this.repos[0] = response.data;
+      console.log(this.repos[0]);
+    });
+    axios.get("https://api.github.com/repos/angular/angular.js").then((response) => {
+      this.repos[1] = response.data;
+      console.log(this.repos[1]);
+    });
+    axios.get("https://api.github.com/repos/emberjs/ember.js").then((response) => {
+      this.repos[2] = response.data;
+      console.log(this.repos[2]);
+    });
+    axios.get("https://api.github.com/repos/sveltejs/svelte").then((response) => {
+      this.repos[3] = response.data;
+      console.log(this.repos[3]);
+    });
+    axios.get("https://api.github.com/repos/facebook/react").then((response) => {
+      this.repos[4] = response.data;
+      console.log(this.repos[4]);
     });
   },
   methods: {},
