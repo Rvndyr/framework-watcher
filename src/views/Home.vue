822f12b2947e399589d04c18595188b48fdf8184
Randy Rodriguez <Randyr2125@msn.com>
2021-08-09 22:03:16 -0400

    refactored the axios calls and the rendered table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="Home">
-    <div>{{ repos.length }}</div>
     <div v-if="repos.length === 5">
       <table class="table table-dark">
         <thead>
@@ -20,34 +19,6 @@
             <td>{{ repo.forks }}</td>
             <td>{{ repo.watchers_count }}</td>
           </tr>
-          <!-- <tr>
-            <th scope="row">2</th>
-            <td>{{ repos[1].name }}</td>
-            <td>{{ repos[1].stargazers_count }}</td>
-            <td>{{ repos[1].forks }}</td>
-            <td>{{ repos[1].watchers_count }}</td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>{{ repos[2].name }}</td>
-            <td>{{ repos[2].stargazers_count }}</td>
-            <td>{{ repos[2].forks }}</td>
-            <td>{{ repos[2].watchers_count }}</td>
-          </tr>
-          <tr>
-            <th scope="row">4</th>
-            <td>{{ repos[3].name }}</td>
-            <td>{{ repos[3].stargazers_count }}</td>
-            <td>{{ repos[3].forks }}</td>
-            <td>{{ repos[3].watchers_count }}</td>
-          </tr>
-          <tr>
-            <th scope="row">5</th>
-            <td>{{ repos[4].name }}</td>
-            <td>{{ repos[4].stargazers_count }}</td>
-            <td>{{ repos[4].forks }}</td>
-            <td>{{ repos[4].watchers_count }}</td>
-          </tr> -->
         </tbody>
       </table>
     </div>
@@ -69,25 +40,13 @@ export default {
   },
   methods: {
     LoadData() {
-      axios.get("https://api.github.com/repos/vuejs/vue").then((response) => {
-        this.repos.push(response.data);
-        console.log(this.repos[0]);
-      });
-      axios.get("https://api.github.com/repos/angular/angular.js").then((response) => {
-        this.repos.push(response.data);
-        console.log(this.repos[1]);
-      });
-      axios.get("https://api.github.com/repos/emberjs/ember.js").then((response) => {
-        this.repos.push(response.data);
-        console.log(this.repos[2]);
-      });
-      axios.get("https://api.github.com/repos/sveltejs/svelte").then((response) => {
-        this.repos.push(response.data);
-        console.log(this.repos[3]);
-      });
-      axios.get("https://api.github.com/repos/facebook/react").then((response) => {
-        this.repos.push(response.data);
-        console.log(this.repos[4]);
+      let repoUrl = ["vuejs/vue", "emberjs/ember.js", "sveltejs/svelte", "facebook/react", "angular/angular"];
+      repoUrl.forEach((url) => {
+        axios.get(`https://api.github.com/repos/${url}`).then((response) => {
+          console.log(response.data);
+          this.repos.push(response.data);
+          console.log(this.repos);
+        });
       });
     },
   },
